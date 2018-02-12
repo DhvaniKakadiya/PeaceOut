30fa865aab3435c87b90a7d7822f694d59a95755
Dhvani <kakadiyadhvani@gmail.com>
2018-02-12 14:20:37 -0500

    V1

---
+++ app/src/main/java/com/musicplayer/lysic/MusicController.java
@@ -0,0 +1,25 @@
+package com.musicplayer.lysic;
+
+/**
+ * Created by Asha on 2/6/2018.
+ */
+
+import android.content.Context;
+import android.widget.MediaController;
+
+/*
+ * This is demo code to accompany the Mobiletuts+ series:
+ * Android SDK: Creating a Music Player
+ *
+ * Sue Smith - February 2014
+ */
+
+public class MusicController extends MediaController {
+
+    public MusicController(Context c){
+        super(c);
+    }
+
+    public void hide(){}
+
+}
---
+++ app/src/main/java/com/musicplayer/lysic/Song.java
@@ -0,0 +1,23 @@
+package com.musicplayer.lysic;
+
+/**
+ * Created by Asha on 2/6/2018.
+ */
+
+public class Song {
+
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long songID, String songTitle, String songArtist){
+        id=songID;
+        title=songTitle;
+        artist=songArtist;
+    }
+
+    public long getID(){return id;}
+    public String getTitle(){return title;}
+    public String getArtist(){return artist;}
+
+}
