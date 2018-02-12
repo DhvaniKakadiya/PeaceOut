471e3661bc498d40a69f8f34bfe1f4e1adcbcc7c
Dhvani <kakadiyadhvani@gmail.com>
2018-02-12 14:06:06 -0500

    V1

---
+++ app/src/main/java/com/musicplayer/lysic/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.musicplayer.lysic;
+
+/**
+ * Created by Asha on 2/5/2018.
+ */
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by AbhiAndroid
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splashfile);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+    }
+}
